15fae26dc80b10a4e54cbc02fdcd7ec5e8ea0ab9
Борис Андрос <borisandros@yandex.ru>
2022-09-12 23:35:30 +0300

    Исправлен класс streammain

--- src/StreamMain.java
+++ src/StreamMain.java
@@ -7,13 +7,12 @@ public class StreamMain {
     public static void main(String[] args) {
 
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        List<Integer> sortedList2 = intList.stream()
+        intList
+                .stream()
                 .filter(x->x>0)
                 .filter(x -> x%2==0)
                 .sorted(Comparator.naturalOrder())
-                .collect(Collectors.toList());
-
-        System.out.println(sortedList2);
+                .forEach(System.out::println);
 
     }
 }
